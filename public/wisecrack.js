48dfcfa61b16f3b4e665b0ca38341e016fcbb091
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2025-08-16 14:05:17 -0700

    swdd

--- public/wisecrack.js
+++ public/wisecrack.js
@@ -4,4 +4,9 @@ function connect_to_room(){
     socket.emit('join-room')
 }
 
+socket.on('kicked', (reason) => {
+    alert(`Uh-oh: ${reason}`)
+    cookieStore.delete('connect.sid')
+})
+
 connect_to_room()
